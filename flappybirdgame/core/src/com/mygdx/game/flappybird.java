6c50e41971d8bd4c9ce87b633cd7eccd3d0948f5
Ayur <ayursarawgi0929@gmail.com>
2020-08-12 19:51:39 +0530

    birds added

---
+++ flappybirdgame/core/src/com/mygdx/game/flappybird.java
@@ -0,0 +1,38 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class flappybird extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture background;
+	Texture[] birds;
+	int flapstate=0;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		background= new Texture("bg.png");
+		birds=new Texture[2];
+		birds[0]= new Texture("bird.png");
+		birds[1]= new Texture("bird2.png");
+	}
+
+	@Override
+	public void render () {
+
+		if (flapstate == 0)
+		{
+			flapstate=1;
+		}else flapstate=0;
+
+		batch.begin();
+		batch.draw(background, 0,0,Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.draw(birds[flapstate], Gdx.graphics.getWidth()/2-birds[flapstate].getWidth()/2, Gdx.graphics.getHeight()/2-birds[flapstate].getHeight()/2);
+		batch.end();
+	}
+
+}
