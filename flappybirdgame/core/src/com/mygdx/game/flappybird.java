317adfd143a43e09a5552371d81679b260d089f3
Ayur <ayursarawgi0929@gmail.com>
2020-08-13 18:07:55 +0530

    moving pipes with jumping bird

--- flappybirdgame/core/src/com/mygdx/game/flappybird.java
+++ flappybirdgame/core/src/com/mygdx/game/flappybird.java
@@ -5,22 +5,29 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.physics.box2d.Fixture;
 
 import java.util.Random;
 
 public class flappybird extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture background, bottompipe[], toppipe[];
+	Texture background, bottompipe, toppipe;
 	Texture[] birds;
 	int flapstate = 0;
 	float birdY;
 	float gravity = (float) 0.8;
 	float velocity = 0;
 	int gamestate = 0;
-	int gap = 250;
+	int gap = 100;
 	Random random;
-	float tubeoffset;
-	int velocitypipe = 0;
+	int numberofpipes=4;
+	float[] tubeoffset=new float[numberofpipes];
+	int velocitypipe = 5;
+	float[] tubeX=new float[numberofpipes];
+float distbtwpipes;
+float max= (float) 0.99;
+	float min= (float) -0.99;
+
 
 	@Override
 	public void create() {
@@ -30,16 +37,26 @@ public class flappybird extends ApplicationAdapter {
 		birds[0] = new Texture("bird.png");
 		birds[1] = new Texture("bird2.png");
 		birdY = Gdx.graphics.getHeight() / 2 - birds[flapstate].getHeight() / 2;
-		bottompipe = new Texture[2];
-		bottompipe[0] = new Texture("bottomtube.png");
-		bottompipe[1] = new Texture("bottomtube.png");
-		toppipe = new Texture[2];
-		toppipe[0] = new Texture("toptube.png");
-		toppipe[1] = new Texture("toptube.png");
 
+		bottompipe = new Texture("bottomtube.png");
+
+
+		toppipe = new Texture("toptube.png");
+
+		distbtwpipes=Gdx.graphics.getWidth()/2;
+
+
+		for(int i=0;i<numberofpipes-1;i++)
+		{
+
+
+		tubeX[i]=Gdx.graphics.getWidth() / 2 - toppipe.getWidth() / 2 +i*distbtwpipes*1.2f ;
 		random = new Random();
-		tubeoffset = Gdx.graphics.getHeight() / 2 - toppipe[0].getHeight() / 2;
-	}
+
+			tubeoffset[i] = (float) ( Math.random() * (max - min + 1) + min) * (Gdx.graphics.getHeight()/2 - toppipe.getHeight()/2 );
+
+
+	}}
 
 	@Override
 	public void render() {
@@ -55,26 +72,42 @@ public class flappybird extends ApplicationAdapter {
 		if (Gdx.input.justTouched()) {
 
 			Gdx.app.log("Touched", "yep");
-			velocity = -18;
+			velocity = -20;
 			gamestate = 1;
-			tubeoffset = (random.nextFloat() * 3.25f - 1f) * (Gdx.graphics.getHeight() / 2 - toppipe[0].getHeight() / 2);
+
 
 		}
 
-		if (birdY <= 0) {
+
+
+
+		if (birdY <= 0 ) {
 			gamestate = 0;
+			velocitypipe=0;
 		}
 
 		if (flapstate == 0) {
 			flapstate = 1;
 		} else flapstate = 0;
 
-		batch.draw(toppipe[0], Gdx.graphics.getWidth() / 2 - toppipe[0].getWidth() / 2 + velocitypipe, Gdx.graphics.getHeight() / 2 + gap / 2 + tubeoffset);
-		batch.draw(bottompipe[0], Gdx.graphics.getWidth() / 2 - bottompipe[0].getWidth() / 2 + velocitypipe, -Gdx.graphics.getHeight() / 2 + gap / 2 + tubeoffset);
-		velocitypipe-=3;
+
+		for(int i=0;i<numberofpipes-1;i++) {
+
+			if(tubeX[i] < -toppipe.getWidth())
+			{tubeoffset[i] = (float) ( Math.random() * (max - min + 1) + min) * (Gdx.graphics.getHeight()/2 - toppipe.getHeight()/2 );
+
+				tubeX[i]= tubeX[i]+ numberofpipes*distbtwpipes - toppipe.getWidth();
+			}else {tubeX[i]-=velocitypipe;}
+
+			batch.draw(toppipe, tubeX[i] +20, Gdx.graphics.getHeight() / 2 + gap / 2 + tubeoffset[i]+100);
+			batch.draw(bottompipe, tubeX[i] +20, -Gdx.graphics.getHeight() / 2 + gap / 2 + tubeoffset[i]+200);
+
+			
+
+		}
 
 			batch.draw(birds[flapstate], Gdx.graphics.getWidth() / 2 - birds[flapstate].getWidth() / 2, birdY);
 			batch.end();
-			
+
 	}
 }
